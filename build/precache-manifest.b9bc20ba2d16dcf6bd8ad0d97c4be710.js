c4fdb5499e056f97770850e40480dd2335407044
MathieuFigari <mathieu.figari@gmail.com>
2022-05-13 19:12:11 +0200

    portfolioOK

---
+++ build/precache-manifest.b9bc20ba2d16dcf6bd8ad0d97c4be710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357317fd884ed9324191280e8d9e86a0",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "4da9c0bf38dafee05f94",
+    "url": "/react-frontend-dev-portfolio/static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "70d9fc736d5a12c0d4c4",
+    "url": "/react-frontend-dev-portfolio/static/css/main.704b061d.chunk.css"
+  },
+  {
+    "revision": "4da9c0bf38dafee05f94",
+    "url": "/react-frontend-dev-portfolio/static/js/2.9538e46b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.9538e46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d9fc736d5a12c0d4c4",
+    "url": "/react-frontend-dev-portfolio/static/js/main.0e6dbb22.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
